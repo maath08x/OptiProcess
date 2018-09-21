0ba05c545d33908764b27ad16551dedebf0b9133
Matheus Sousa <mso97@hotmail.com>
2018-09-20 22:15:17 -0300

    - Inclusão de Produtos.

---
+++ Opti/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using Opti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Opti.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private static Produtos produtos = new Produtos();
+        private static ProdutosFilhos produtosFilhos = new ProdutosFilhos();
+        private static ProdutosModel produtosModel = new ProdutosModel();
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Produtos";
+            ViewBag.AtributosSearch = new List<string>();
+            ViewBag.AtributosGrid = new List<string>();
+            ViewBag.AtributosEditable = new List<string>();
+            ViewBag.AtributosAddDiff = new List<string>();
+
+
+
+            return View();
+        }
+    }
+}
