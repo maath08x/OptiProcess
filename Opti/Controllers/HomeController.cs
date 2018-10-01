cf37c2e32cdc79fb5d9ee304174891f2c6fd930a
PedroFaia <pedro.faia@hotmail.com>
2018-09-30 21:40:54 -0300

    Dash

--- Opti/Controllers/HomeController.cs
+++ Opti/Controllers/HomeController.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Opti.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Opti.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static DashboardModel dashboardModel = new DashboardModel();
+        private static Dashboard dashboard = new Dashboard();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +33,15 @@ namespace Opti.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public JsonResult Dashboard()
+        {
+            //dashboard.pedidoID = Convert.ToInt16(Request.Params["id"]);
+            List<Dashboard> lp = dashboard.Pesquisar();
+
+            return Json(lp, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ Opti/js/Dashboard.js
@@ -0,0 +1,38 @@
+﻿function pesquisarDash() {
+
+    var usuario = window.localStorage.getItem("usuario");
+
+    var sRequest = "Usuario=" + usuario;
+
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var tBody = document.getElementById("tBody");
+            var response = JSON.parse(xhttp.response);
+            var sInner = "";
+            for (var i = 0; i < response.length; i++) {
+
+                var pedidoId = response[i]["pedidoId"];
+                var nome = response[i]["nome"];
+                var leadTime = response[i]["leadTime"];
+                var qntEstoque = response[i]["qntEstoque"];
+                var qntEstoqueReservado = response[i]["qntEstoqueReservado"];
+                var QntPedidoTotal = response[i]["QntPedidoTotal"];
+                var dtPedido = response[i]["dtPedido"];
+                var dtPrevisao = response[i]["dtPrevisao"];
+            }
+        }
+    }
+
+
+    var sURL = "http://" + location.host + "/Home/Dashboard?";
+
+    xhttp.open("GET", sURL, true);
+    xhttp.send();
+
+
+}
+
+
+
